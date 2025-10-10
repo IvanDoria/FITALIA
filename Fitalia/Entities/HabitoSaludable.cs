ad4f1013577521e5b40f42c3c2e13d03fe03f3c0
wDroxl <davidmola2112@gmail.com>
2025-10-09 22:55:03 -0500

    No se me puede olvidar guardar

--- Fitalia/Entities/HabitoSaludable.cs
+++ Fitalia/Entities/HabitoSaludable.cs
@@ -2,5 +2,17 @@
 {
     public class HabitoSaludable
     {
+        private int ID { get; set; }
+
+        private string Nombre { get; set; }
+        private string Descripcion { get; set; }
+
+        private string Beneficio { get; set; }
+
+
+
+
+
+
     }
 }
