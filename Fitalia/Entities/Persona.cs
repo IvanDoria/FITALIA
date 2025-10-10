7de35ecf18bb1a3bbfc1f00eb55590f647cad89a
wDroxl <davidmola2112@gmail.com>
2025-10-09 22:30:04 -0500

    Commit atributos
    Le puse los atributos a las clases Personas y Habitos

--- Fitalia/Entities/Persona.cs
+++ Fitalia/Entities/Persona.cs
@@ -2,5 +2,16 @@
 {
     public class Persona
     {
+        private int ID { get; set; }
+        private string userID { get; set; }
+        private string nombre { get; set; }
+        private string? apellidoPaterno { get; set; }
+        private string? apellidoMaterno { get; set; }
+        private int edad { get; set; }
+        private string? numeroDeTelefono { get; set; }
+        private string sexo { get; set; }
+
+
+
     }
 }
